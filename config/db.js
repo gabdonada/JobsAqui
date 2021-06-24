8bde7cc1865baf4f01d82933d0cd10fd3662a8e6
gabdonada <48109393+gabdonada@users.noreply.github.com>
2021-06-24 01:21:30 -0300

    Arrumando perfumaria

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://gabrieldonada:Batata@123@cluster0.onmcb.mongodb.net/findJobs?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "mongodb+srv://gabrieldonada:job123@cluster0.onmcb.mongodb.net/findJobs?retryWrites=true&w=majority"}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/findJobs"}
 }
